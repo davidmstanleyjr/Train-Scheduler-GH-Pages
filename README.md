7be9fe24d28f9f784b8b425fca5ce8e5035710d5
David M. Stanley Jr <davidmstanleyjr@gmail.com>
2019-09-30 21:37:42 -0400

    copy and pasted files from original train scheduler repo

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Train-Scheduler-GH-Pages
+# Train-Scheduler
+
+This is my train scheduler app that I built for my bootcamp. I'm utilizing firebase to store information about train times. The train data that is generated is supposed to remain visible after the page is refreshed.
---
+++ assets/javascript/app.js
@@ -0,0 +1,66 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyAeCs6YDbOslXK1QOzZ1SSgl5ErIvemaNA",
+    authDomain: "david-s-first-firebase-project.firebaseapp.com",
+    databaseURL: "https://david-s-first-firebase-project.firebaseio.com",
+    projectId: "david-s-first-firebase-project",
+    storageBucket: "",
+    messagingSenderId: "257188751768",
+    appId: "1:257188751768:web:c70b0914bf0d0b6d8a23ed"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+
+var database = firebase.database();
+
+var trainName = "";
+var destination = "";
+var trainTime = "";
+var frequency = "";
+
+// onClick event
+$("#train-form").submit( function (event) {
+    event.preventDefault();
+    trainName = $("#train-name-input").val().trim();
+    destination = $("#destination-input").val().trim();
+    trainTime = $("#train-time-input").val().trim();
+    frequency = $("#frequency-input").val().trim();
+
+    database.ref().push({
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency
+
+    });
+    $("#train-name-input").val("");
+    $("#destination-input").val("");
+    $("#train-time-input").val("");
+    $("#frequency-input").val("");
+});
+//Puts firebase into HTML.
+database.ref().on("child_added", function (snapshot) {
+    var trainName = snapshot.val().trainName;
+    var destination = snapshot.val().destination;
+    var trainTime = snapshot.val().frequency;
+
+    //converts the times
+    var firstTrainTime = moment(trainTime, "HH:mm a").subtract(1, "years");
+    var nextTrainTime = moment().diff(firstTrainTime, "minutes");
+    var timeRemaining = nextTrainTime % frequency;
+    var timeUntilTheNextTrain = frequency - timeRemaining;
+    var theTrainAfterThat = moment().add(timeUntilTheNextTrain, "minutes");
+    //calculates everything and creates new slots for the trains using bootstrap properties.
+    var trainSlots = $("<tr>").append(
+        $("<td>").text(trainName),
+        $("<td>").text(destination),
+        $("<td>").text(frequency),
+        $("<td>").text(theTrainAfterThat),
+        $("<td>").text(timeUntilTheNextTrain)
+
+    )
+
+    $("#trains > tbody").append(trainSlots);
+
+
+});
